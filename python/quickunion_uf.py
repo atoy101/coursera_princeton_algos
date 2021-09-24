c964d94024d3a88d1313b27866ec9479ce13c90f
Alexander Toy <atoy@anelto.com>
2021-09-24 15:21:23 -0500

    quickunion to N+M*lg*N

--- python/quickunion_uf.py
+++ python/quickunion_uf.py
@@ -8,49 +8,48 @@ class QuickunionUF:
 
 	def __init__(self, N):		#O(n) time
 		self.data = []
+		self.weight = []
 		for i in range(N):
-			self.data.append(i);
-			
+			self.data.append(i)
+			self.weight.append(1)
+	
+	def root(self, i):
+		while(self.data[i]!=i):
+			self.data[i]=self.data[self.data[i]]
+			i= self.data[i]
+		return i
+		
 	def union(self,num1,num2):		#O(1) time
-		self.data[num1] = num2;
-			
-	def connected(self,num1,num2):		#O(n) time worst case from O(2n) by adding if statement to check if head==num1 or num2 
-		next = self.data[num1]
-		head = num1;
-		while(head!=next):
-			head = next
-			next = self.data[head]
-			if (head==num2):
-				return True
-		num1_root=head
-		
-		next = self.data[num2]
-		head = num2;
-		while(head!=next):
-			head = next
-			next = self.data[head]
-			if (head==num1):
-				return True
-		num2_root=head;
-		
-		if (num2_root==num1_root):
-			return True;
+		i = self.root(num1)
+		j = self.root(num2)
+		if (i==j):
+			return
+		if (self.weight[i]>self.weight[j]):
+			self.data[j]=i
+			self.weight[i]+=self.weight[j]
 		else:
-			return False;
+			self.data[i]=j
+			self.weight[j]+=self.weight[i]
+			
+	def connected(self,num1,num2):		#O(n) 
+		return self.root(num1) == self.root(num2)
 			
 	def print_data(self):		#O(n) time
 		for i in range(len(self.data)):
-			print(str(i)+","+str(self.data[i]))
+			print(str(i)+","+str(self.data[i])+","+str(self.weight[i]))
+			
 			
 
 if __name__ == "__main__":
 	
-	x= QuickunionUF(10)
-	x.union(0,1)
-	x.union(7,8)
-	x.union(1,8)
-	x.union(4,1)
+	x= QuickunionUF(1000000)
+	for i in range(1000000):
+		if i%2==0:
+			x.union(i,i+1)
+	for i in range(1000000):
+		if i%5==0:
+			x.union(i,i+3)
 	x.print_data()
-	y = x.connected(7,4)
+	y = x.connected(2,100000)
 	print(y)
 	
