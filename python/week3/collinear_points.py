aa3bb4935afaa4253a7f5c212415ac7236d72020
Alexander Toy <atoy@anelto.com>
2021-10-05 13:07:44 -0500

    collinear points finished

--- python/week3/collinear_points.py
+++ python/week3/collinear_points.py
@@ -13,24 +13,24 @@ class Point:
 	
 	def __lt__(self,other):
 		if self.x<other.x:
-			return 1
+			return True
 		elif self.x>other.x:
-			return -1
+			return False
 		elif self.y<other.y:
-			return 1
+			return True
 		elif self.y>other.y:
-			return -1
+			return False
 		else:
 			return 0
 	def __gt__(self,other):
 		if self.x<other.x:
-			return -1
+			return False
 		elif self.x>other.x:
-			return 1
+			return True
 		elif self.y<other.y:
-			return -1
+			return False
 		elif self.y>other.y:
-			return 1
+			return True
 		else:
 			return 0
 			
@@ -80,7 +80,7 @@ def plot_lines(array):
 if __name__=="__main__":
 	num_points = 0
 	points = []
-	if len(sys.argv) >= 1:
+	if len(sys.argv) > 1:
 		with open(sys.argv[1], 'r') as file:
 			num_points = int(file.readline())
 			for line in file:
@@ -89,37 +89,53 @@ if __name__=="__main__":
 				points.append(Point(int(p[0]),int(p[1])))
 	else: 
 		print("need input")
+		exit()
 	
 	plot_points(points)
 	lines = []
+	
 	for p in points:
 		cmp_key = cmp_to_key(p.cmp_slopes)
-		sorted_p = sorted(points,key=cmp_key)
-		sorted_p=sorted_p[1:]
+		sorted_p = sorted(points,key=cmp_key)		#sort points by slope compared to point p
+		sorted_p.remove(p)							#remove the origin from the sorted list
+		origin = p
 		count=1
 		prev_slope=None
 		temp_points=[]
-		for i,q in enumerate(sorted_p):
+		slopes =[]
+		
+		for i,q in enumerate(sorted_p):				#if 3 same slopes in a row, add them all to a list, sort by (x,y) and take the furthest apart
 			slope=p.slopeto(q)
+			slopes.append(slope)
 			if slope==prev_slope:
 				count+=1
 				temp_points.append(q)
 			if slope!=prev_slope:
-				if count>=2:
-					print(temp_points)
-					temp_points=sorted(temp_points)
-					print(temp_points)
-					print()
+				if count>=3:	
+					temp_points.append(origin)
+					temp_points.sort()
 					temp_point = [temp_points[0],temp_points[-1]]
+					print(temp_points,"temp points")
 					if  temp_point in lines:
 						pass
 					else:
-						#lines.append([p,sorted_p[i-1]])
 						lines.append(temp_point)
 				count = 1
 				temp_points=[]
 			prev_slope=slope
 			temp_points.append(q)
+		temp_points = temp_points[0:len(temp_points)-1]
+		
+		
+		if count>=3:	
+					temp_points.append(origin)
+					temp_points.sort()
+					temp_point = [temp_points[0],temp_points[-1]]
+					print(temp_points,"temp points")
+					if  temp_point in lines:
+						pass
+					else:
+						lines.append(temp_point)
 	
 	#print(lines)
 	for line in lines:
